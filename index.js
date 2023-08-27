38395c156ddaf32a0342b237d41fa4c2a16db2ac
shun916 <118881278+code-polaris044@users.noreply.github.com>
2023-08-27 19:15:49 +0900

    css追加

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 "use strict";
 
-import $ from "jquery";
+// import $ from "jquery";
+// import "jquery";
+// export default window.jQuery;
+
 // import "slick-carousel";
 
-$("h1").css("color", "red");
+// $("h1").css("color", "red");
