6629d5772de7a48b13d83ed494cf447c381856e6
shun916 <118881278+code-polaris044@users.noreply.github.com>
2023-10-04 20:40:06 +0900

    jqery

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 "use strict";
 
-// import $ from "jquery";
-// import "jquery";
-// export default window.jQuery;
-
-// import "slick-carousel";
-
-// $("h1").css("color", "red");
+$(document).ready(function () {
+  $(".slider").slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    autoplay: true, // 自動再生
+    arrows: true, // 前後の矢印を表示
+    dots: true, // ページネーションドットを表示
+    prevArrow: '<i class="fas fa-chevron-left"></i>',
+    nextArrow: '<i class="fas fa-chevron-right"></i>',
+  });
+});
